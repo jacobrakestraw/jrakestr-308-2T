30bf642a026aa0e265b51d3a3f96b04a0fe6499e
jacobrakestraw <46944076+jacobrakestraw@users.noreply.github.com>
2019-02-06 14:33:51 -0800

    Fixed typing in Multiply

--- src/Multiply.java
+++ src/Multiply.java
@@ -2,7 +2,7 @@ public class Multiply{
 	public static void Main(String[] args){
 		Mult(1,2);
 	}
-	public static int Mult(numOne, numTwo){
+	public static int Mult(int numOne, int numTwo){
 		return numOne * numTwo;
 	}
 }
